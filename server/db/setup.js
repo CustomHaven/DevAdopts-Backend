9f0bf40603b71cb13187c20f6991e91dfcbab84d
Mohamed <mmuseaden@gmail.com>
2024-08-28 20:42:05 +0100

    Fix database/setup.sql

--- server/db/setup.js
+++ server/db/setup.js
@@ -3,8 +3,10 @@ const fs = require("fs");
 const path = require("path");
 
 const db = require("./connect");
+console.log("waht is __dirname?", __dirname)
+const pathJoin = path.join(__dirname, '../../database/setup.sql');
 
-const pathJoin = path.join(__dirname + "../../database/setup.sql");
+console.log("The path join fix", pathJoin); // Outputs the absolute path to setup.sql
 const sql = fs.readFileSync(pathJoin).toString();
 
 
