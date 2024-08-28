aa7eaa5e156f7d698d77337ed1599c280d4613a7
Mohamed <mmuseaden@gmail.com>
2024-08-28 22:44:49 +0100

    PASS unit test for dogs controller

--- server/db/setup.js
+++ server/db/setup.js
@@ -3,12 +3,10 @@ const fs = require("fs");
 const path = require("path");
 
 const db = require("./connect");
-console.log("waht is __dirname?", __dirname)
+
 const pathJoin = path.join(__dirname, '../../database/setup.sql');
 
-console.log("The path join fix", pathJoin); // Outputs the absolute path to setup.sql
 const sql = fs.readFileSync(pathJoin).toString();
-
 
 db.query(sql)
     .then(() => {
