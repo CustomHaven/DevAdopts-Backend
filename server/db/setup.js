c18c5580651499bff9eea867fe365b14fc36d947
Mohamed <mmuseaden@gmail.com>
2024-08-28 13:20:01 +0100

    Add better defined scripts path for server/package.json

---
+++ server/db/setup.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+
+const db = require("./connect");
+
+const pathJoin = path.join(__dirname + "../../database/setup.sql");
+const sql = fs.readFileSync(pathJoin).toString();
+
+
+db.query(sql)
+    .then(() => {
+        db.end();
+        console.log("Set-up complete");
+    })
+    .catch(err => console.log(err));
